473a81471f8e12c1518aa9b1435f70c2b11ea25e
Vinh Le <vinh@vertics.co>
2020-05-05 14:25:49 +0300

    refactor: add listener on port 3000

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
 	res.status(200).json({message: 'Hello world'})
 })
+
+app.listen(4000, () => {
+	console.log('App is listening on port 4000')
+})
